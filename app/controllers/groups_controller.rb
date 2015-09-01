f3a8fefc83b4fbb08a9c642e392e8ed32556dff9
Jhougo <jhougo7928@gmail.com>
2015-09-01 13:36:50 +0800

    31

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,24 @@
 class GroupsController < ApplicationController
     def index
+        flash[:notice] = "早安！你好！"
+        flash[:alert] = "晚安！該睡了！"
+        flash[:warning] = "這是 warning 訊息！"
+    end
+    def show
+    end
+    
+    def new
+    end
+    
+    def edit
+    end
+    
+    def create
+    end
+    
+    def update
+    end
+    
+    def destroy
     end
 end
