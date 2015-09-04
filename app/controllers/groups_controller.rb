38684e169105422b8a05a74df5d833de6c76c13d
Jhougo <jhougo7928@gmail.com>
2015-09-04 21:46:39 +0800

    9.3

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,19 +1,28 @@
 class GroupsController < ApplicationController
     def index
-        flash[:notice] = "早安！你好！"
-        flash[:alert] = "晚安！該睡了！"
-        flash[:warning] = "這是 warning 訊息！"
+        @groups = Group.all
+    #     flash[:notice] = "早安！你好！"
+    #     flash[:alert] = "晚安！該睡了！"
+    #     flash[:warning] = "這是 warning 訊息！"
     end
     def show
     end
     
     def new
+        @group = Group.new
     end
     
     def edit
     end
     
     def create
+        @group = Group.create(group_params)
+
+        if @group.save
+            redirect_to groups_path
+        else
+            render :new
+        end
     end
     
     def update
@@ -21,4 +30,9 @@ class GroupsController < ApplicationController
     
     def destroy
     end
+end
+private
+
+def group_params 
+  params.require(:group).permit(:title, :description)
 end
